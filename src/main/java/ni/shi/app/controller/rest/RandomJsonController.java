d5e9768b56c275e3b3be4c7534edc8cb20add6c5
Fkame <nikitos_flame@mail.ru>
2022-09-22 22:04:12 +0300

    Починил контроллер

--- src/main/java/ni/shi/app/controller/rest/RandomJsonController.java
+++ src/main/java/ni/shi/app/controller/rest/RandomJsonController.java
@@ -3,6 +3,7 @@ package ni.shi.app.controller.rest;
 import ni.shi.app.controller.dto.FullyRandomJsonParamsDto;
 import ni.shi.app.service.json.generator.completely.CompletelyRandomJsonGenerator;
 import ni.shi.app.service.json.generator.template.by_fields_types.JsonGeneratorByFieldType;
+import ni.shi.app.service.json.generator.template.by_input_types.JsonGeneratorByInputTypes;
 import ni.shi.app.service.json.generator.template.embedded.JsonByTemplateGenerator;
 import ni.shi.app.utils.StructureComplexity;
 import org.springframework.http.MediaType;
@@ -10,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(value = "v1/rest/json", produces = MediaType.APPLICATION_JSON_VALUE)
-
+@RestController
 public class RandomJsonController {
 
     @GetMapping(value = "/random/embedded-template")
@@ -34,9 +35,19 @@ public class RandomJsonController {
         return ResponseEntity.ok(json);
     }
 
-    @PostMapping(value = "/random/fill-only")
-    public ResponseEntity<String> getRandomJsonFromUserTemplate(@RequestBody String jsonTemplate) {
+    @PostMapping(value = "/random/by-field-types")
+    public ResponseEntity<String> regenerateJsonByFieldsTypes(@RequestBody String jsonTemplate) {
         JsonGeneratorByFieldType jsonGenerator = new JsonGeneratorByFieldType();
+        String json = jsonGenerator.generateByCustomTemplate(jsonTemplate);
+        if (json == null) {
+            return ResponseEntity.internalServerError().body("Cannot create json, sorry(");
+        }
+        return ResponseEntity.ok(json);
+    }
+
+    @PostMapping(value = "/random/by-input-types")
+    public ResponseEntity<String> regenerateJsonByInputTypes(@RequestBody String jsonTemplate) {
+        JsonGeneratorByInputTypes jsonGenerator = new JsonGeneratorByInputTypes();
         String json = jsonGenerator.generateByCustomTemplate(jsonTemplate);
         if (json == null) {
             return ResponseEntity.internalServerError().body("Cannot create json, sorry(");
